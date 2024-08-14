281568a2e9b3eab19784ef1c15b9521e939f2bd2
vinhe21rjqwnr1kl <hosuzune@gmail.com>
2024-08-14 10:38:17 +0700

    two commit

---
+++ custom-addons/real-estate-ads/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    "name": "Technical Support Ticket",
+    "version" : "1.0",
+    "author" : "vinhcodeacademy@gmail.com",
+    "description": """
+     Technical Support Ticket module to show available properties
+    """,
+    "category": "Sales",
+    "depends": ['base','mail'],
+    "data": [
+
+        'security/ir.model.access.csv',
+        'security/res._groups.xml',
+        'security/model_access.xml',
+        'security/ir_rule.xml',
+                  
+        'views/ticket_view.xml',
+        'views/ticket_type_view.xml',
+        'views/ticket_status_view.xml',
+        'views/ticket_respone_view.xml',
+        'views/menu_items.xml',
+
+        'data/mail_template.xml',
+
+    ],
+    "installable" : True,
+    "application" : True,
+    "license" : "LGPL-3",
+    'auto_install': False,
+}
